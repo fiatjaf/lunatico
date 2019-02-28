96b08f9a4fe062c3a59b3985bb89384883620bcb
fiatjaf <fiatjaf@gmail.com>
2019-02-28 02:22:56 +0000

    value conversion working.

---
+++ main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/aarzilli/golua/lua"
+	"github.com/kr/pretty"
+)
+
+func main() {
+	L := lua.NewState()
+	L.OpenLibs()
+
+	PushAny(L, map[string]string{"x": "x", "y": "y", "z": "z"})
+	PushAny(L, []interface{}{1, true, false, nil, "weq"})
+	PushAny(L, []interface{}{
+		"another-item1",
+		"another-item2",
+		map[string]string{"xxx": "vxxx", "yyy": "vyyy", "zzz": "vzzz"},
+		map[int]bool{7: true, 8: false},
+		map[string]interface{}{
+			"key1": 777777777,
+			"key2": []interface{}{1, 2, 3, "sete"},
+		},
+	})
+
+	pretty.Log(GetFullStack(L))
+}
+
+// utils
+func GetFullStack(L *lua.State) []interface{} {
+	tip := L.GetTop()
+	values := make([]interface{}, tip)
+	for i := 1; i <= tip; i++ {
+		v := ReadAny(L, i)
+		values[i-1] = v
+	}
+	return values
+}
+
+func printStackTypes(L *lua.State) {
+	tip := L.GetTop()
+	values := make([]interface{}, tip)
+	for i := 1; i <= tip; i++ {
+		v := L.Type(i)
+		values[i-1] = v
+	}
+	fmt.Println(values...)
+}
+
+// read stuff
+func ReadAny(L *lua.State, pos int) interface{} {
+	switch L.Type(pos) {
+	case lua.LUA_TNIL:
+		return nil
+	case lua.LUA_TNUMBER:
+		return L.ToNumber(pos)
+	case lua.LUA_TBOOLEAN:
+		return L.ToBoolean(pos)
+	case lua.LUA_TSTRING:
+		return L.ToString(pos)
+	case lua.LUA_TTABLE:
+		return ReadTable(L, pos)
+	case lua.LUA_TFUNCTION:
+		return nil
+	}
+	return nil
+}
+
+func ReadString(L *lua.State, pos int) (v string) {
+	switch L.Type(pos) {
+	case lua.LUA_TNUMBER:
+		return fmt.Sprint(L.ToNumber(pos))
+	case lua.LUA_TBOOLEAN:
+		return fmt.Sprint(L.ToBoolean(pos))
+	case lua.LUA_TSTRING:
+		return L.ToString(pos)
+	}
+	return ""
+}
+
+func ReadTable(L *lua.State, pos int) interface{} {
+	if pos < 0 {
+		pos = L.GetTop() + 1 + pos
+	}
+
+	var object = make(map[string]interface{})
+	var slice []interface{}
+
+	isArray := true
+	size := L.ObjLen(pos)
+	if size == 0 {
+		isArray = false
+	} else {
+		slice = make([]interface{}, size)
+	}
+
+	L.PushNil()
+
+	for L.Next(pos) != 0 {
+		val := ReadAny(L, -1)
+		L.Pop(1)
+
+		// array
+		if isArray {
+			if index := L.ToInteger(-1); index != 0 && index <= int(size) {
+				slice[index-1] = val
+			} else {
+				isArray = false
+			}
+		}
+
+		// object
+		key := ReadString(L, -1)
+		object[key] = val
+	}
+
+	if isArray {
+		return slice
+	} else {
+		return object
+	}
+}
+
+// push stuff
+func PushMap(L *lua.State, m map[string]interface{}) {
+	L.CreateTable(0, len(m))
+	for k, v := range m {
+		PushAny(L, k)
+		PushAny(L, v)
+		L.RawSet(-3)
+	}
+}
+
+func PushSlice(L *lua.State, s []interface{}) {
+	L.CreateTable(len(s), 0)
+	for i, v := range s {
+		PushAny(L, v)
+		L.RawSeti(-2, i+1)
+	}
+}
+
+func PushAny(L *lua.State, ival interface{}) {
+	rv := reflect.ValueOf(ival)
+	switch rv.Kind() {
+	case reflect.String:
+		L.PushString(rv.String())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		L.PushNumber(float64(rv.Int()))
+	case reflect.Uint, reflect.Uintptr, reflect.Uint8,
+		reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		L.PushNumber(float64(rv.Uint()))
+	case reflect.Float32, reflect.Float64:
+		L.PushNumber(rv.Float())
+	case reflect.Bool:
+		L.PushBoolean(rv.Bool())
+	case reflect.Slice:
+		size := rv.Len()
+		slice := make([]interface{}, size)
+		for i := 0; i < size; i++ {
+			slice[i] = rv.Index(i).Interface()
+		}
+		PushSlice(L, slice)
+	case reflect.Map:
+		m := make(map[string]interface{}, rv.Len())
+		for _, key := range rv.MapKeys() {
+			m[fmt.Sprint(key)] = rv.MapIndex(key).Interface()
+		}
+		PushMap(L, m)
+	default:
+		L.PushNil()
+	}
+}
